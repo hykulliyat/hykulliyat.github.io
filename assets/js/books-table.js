144686833f6f4996dbaebcf668f0e2b1fba117e9
hykulliyat <sevinsaygi3080@gmail.com>
2026-05-18 09:04:13 +0300

    commit

--- assets/js/books-table.js
+++ assets/js/books-table.js
@@ -71,12 +71,9 @@ async function loadBooksTable() {
             const tdPdf = document.createElement('td');
             if (pdfPath) {
                 let processedPdfPath = pdfPath.replace('kuranvebilim.github.io', 'hykulliyat.github.io');
-                // Replace underscores with hyphens in directory name
                 processedPdfPath = processedPdfPath.replace('Harun_Yahya_Kitaplar', 'Harun-Yahya-Kitaplar');
-                // URL encode only the filename portion
                 const urlParts = processedPdfPath.split('/');
                 const filename = urlParts.pop();
-                // Use filename as-is from CSV (don't modify it)
                 const encodedFilename = encodeURIComponent(filename);
                 processedPdfPath = urlParts.join('/') + '/' + encodedFilename;
                 tdPdf.innerHTML = `<a href="https://hykulliyat.github.io/pdf-viewer/?file=${processedPdfPath}" rel="alternate bookmark nofollow" hreflang=tr type=application/pdf target="_blank"><img src="/assets/img/pdf.png" alt="indir" title="İNDİR PDF"></a>`;
@@ -87,12 +84,9 @@ async function loadBooksTable() {
             const tdEpub = document.createElement('td');
             if (epubPath) {
                 let processedEpubPath = epubPath.replace('kuranvebilim.github.io', 'hykulliyat.github.io');
-                // Replace underscores with hyphens in directory name
                 processedEpubPath = processedEpubPath.replace('Harun_Yahya_Kitaplar', 'Harun-Yahya-Kitaplar');
-                // URL encode only the filename portion
                 const urlParts = processedEpubPath.split('/');
                 const filename = urlParts.pop();
-                // Use filename as-is from CSV (don't modify it)
                 const encodedFilename = encodeURIComponent(filename);
                 processedEpubPath = urlParts.join('/') + '/' + encodedFilename;
                 tdEpub.innerHTML = `<a href="https://hykulliyat.github.io/epub-viewer/?file=${processedEpubPath}" rel="alternate bookmark nofollow" hreflang=tr type=application/epub+zip target="_blank"><img src="/assets/img/epub.png" alt="oku" title="OKU EPUB"></a>`;
@@ -103,14 +97,10 @@ async function loadBooksTable() {
             const tdDocx = document.createElement('td');
             if (docPath) {
                 let processedDocPath = docPath.replace('kuranvebilim.github.io', 'hykulliyat.github.io');
-                // Replace underscores with hyphens in directory name
                 processedDocPath = processedDocPath.replace('Harun_Yahya_Kitaplar', 'Harun-Yahya-Kitaplar');
-                // Change /doc/ to /doc/ and ensure .docx extension
                 processedDocPath = processedDocPath.replace(/\.doc$/, '.docx');
-                // URL encode only the filename portion
                 const urlParts = processedDocPath.split('/');
                 const filename = urlParts.pop();
-                // Use filename as-is from CSV (don't modify it)
                 const encodedFilename = encodeURIComponent(filename);
                 processedDocPath = urlParts.join('/') + '/' + encodedFilename;
                 tdDocx.innerHTML = `<a href="https://hykulliyat.github.io/docx-viewer/?file=${processedDocPath}" rel="alternate bookmark nofollow" hreflang=tr type=application/vnd.openxmlformats-officedocument.wordprocessingml.document target="_blank"><img src="/assets/img/odt.png" alt="indir" title="YAZDIR DOCX"></a>`;
